e04c3b6bcd6ca753adc2602999bd169488386b4d
Roberto-Carlos1 <rakotomalalacarlos14@gmail.com>
2026-06-17 10:22:10 +0300

    sprint2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('achat', 'Achat::index');
+$routes->post('achat/ajouter', 'Achat::ajouter');
+$routes->post('achat/cloturer', 'Achat::cloturer');
+
+// Route temporaire pour tester si la page choix de caisse n'est pas encore faite
+$routes->get('test-caisse/(:num)', 'Achat::testCaisse/$1');
