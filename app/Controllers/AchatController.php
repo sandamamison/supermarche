9ea63c254dd5d3a27d37d59e5619cb9b60aa2f86
Sanda <sandamamison1@gmail.com>
2026-06-17 10:44:24 +0300

    fin

--- app/Controllers/AchatController.php
+++ app/Controllers/AchatController.php
@@ -4,11 +4,31 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\Achat;
+use App\Models\Produit;
+use App\Models\Caisse;
 
 class AchatController extends BaseController
 {
     public function index()
     {
-        //
+        $caisse_id = session()->get('id_caisse');
+
+        if (empty($caisse_id)) {
+            return redirect()->to('/');
+        }
+
+        $model_achat = new Achat();
+        $model_produit = new Produit();
+        $model_caisse = new Caisse();
+
+        $data = [
+            'caisse_id' => $caisse_id,
+            'achats' => $model_achat->where('id_caisse', $caisse_id)->findAll(),
+            'produits' => $model_produit->findAll(),
+            'caisse' => $model_caisse->find($caisse_id)
+        ];
+
+        return view('achats', $data);
     }
 }
